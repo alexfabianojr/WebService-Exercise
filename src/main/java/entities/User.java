abcfb87bc46a63642c614f0412a482108f0d7469
Alex Fabiano Baptista da Silva Junior <alexfabianojr@gmail.com>
2020-06-05 03:28:05 -0300

    2

---
+++ src/main/java/entities/User.java
@@ -0,0 +1,56 @@
+package entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+
+@Entity
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+
+    public User() { }
+
+    public User(Long id, String name, String email, String phone, String password) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    public Long getId()                             { return id; }
+    public String getName()                         { return name; }
+    public String getEmail()                        { return email; }
+    public String getPhone()                        { return phone; }
+    public String getPassword()                     { return password; }
+
+    public void setId(Long id)                      { this.id = id; }
+    public void setName(String name)                { this.name = name; }
+    public void setEmail(String email)              { this.email = email; }
+    public void setPhone(String phone)              { this.phone = phone; }
+    public void setPassword(String password)        { this.password = password; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return getId().equals(user.getId());
+    }
+
+    @Override
+    public int hashCode() { return Objects.hash(getId()); }
+}
